58be010ad1adde79e6acbc2f799a90d6b9c0bcac
Cuong Tran <trquoccuong@gmail.com>
2019-10-03 09:46:35 +0700

    update

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const prerender = require('prerender');
 const server = prerender({
   chromeFlags: [ '--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars' ],
-  forwardHeaders: true,
   chromeLocation: '/usr/bin/chromium-browser'
 });
 
-server.use(require('prerender-request-blacklist'));
-server.use(prerender.blacklist());
+server.use(prerender.sendPrerenderHeader());
+server.use(prerender.removeScriptTags());
+// server.use(require('prerender-request-blacklist'));
+// server.use(prerender.blacklist());
 server.use(prerender.httpHeaders());
 server.use(require('prerender-redis-cache'));
 
